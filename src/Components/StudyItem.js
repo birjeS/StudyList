975555c46da5b312109da4819ca73f07d3e55520
Birje <birje.solatte@voco.ee>
2023-01-26 12:51:15 +0200

    New Components

---
+++ src/Components/StudyItem.js
@@ -0,0 +1,15 @@
+import "./StudyItem.css"
+
+const StudyItem =  () => {
+    return (
+        <div className="study-item">
+            <div>Date</div>
+            <div className="study-item__description">
+                <h2>StudyPlanner</h2>
+                <div className="study-item__list">List</div>
+            </div>
+        </div>
+    )
+}
+
+export default StudyItem
