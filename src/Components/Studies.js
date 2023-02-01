d5bc65b6549be28e12fcc90b099bd091441fa239
Birje <birje.solatte@voco.ee>
2023-02-01 11:10:52 +0200

    Added prompt

---
+++ src/Components/Studies.js
@@ -0,0 +1,14 @@
+import './Studies.css'
+import StudyItem from "./StudyItem";
+import Card from "./Card";
+
+function Studies(props) {
+    return (
+        <Card className="studies">
+            <StudyItem StudyData={props.studies[0]}></StudyItem>
+            <StudyItem StudyData={props.studies[1]}></StudyItem>
+        </Card>
+    )
+}
+
+export default Studies
