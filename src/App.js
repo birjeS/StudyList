285db1aa7a580bbc89dd7f9609eca2862d529e3e
Birje <birje.solatte@voco.ee>
2023-02-01 09:18:00 +0200

    furnishing the component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import './App.css';
 
 import StudyItem from "./Components/StudyItem";
 const App = () => {
+    const study = [
+        {
+        date: new Date (2023, 1, 26),
+        titl: 'Järelevastamine',
+        list: 'Medium'
+    },
+        {
+            date: new Date (2023, 1, 10),
+            titl: 'Kodutöö',
+            list: 'High'
+        }
+]
   return  (
       <div>
-        <StudyItem></StudyItem>
+        <StudyItem StudyData={studies[0]}></StudyItem>
+          <StudyItem StudyData={studies[1]}></StudyItem>
       </div>
-  )
+  );
 }
 export default App;
+
+
--- src/Components/StudyItem.js
+++ src/Components/StudyItem.js
@@ -1,12 +1,14 @@
 import "./StudyItem.css"
 
-const StudyItem =  () => {
+const StudyItem =  (props) => {
+    console.log(props)
+
     return (
         <div className="study-item">
-            <div>Date</div>
+            <div>{props.StudyData.date.toString()}</div>
             <div className="study-item__description">
-                <h2>StudyPlanner</h2>
-                <div className="study-item__list">List</div>
+                <h2>{props.StudyData.title}</h2>
+                <div className="study-item__list">{props.StudyData.list}</div>
             </div>
         </div>
     )
