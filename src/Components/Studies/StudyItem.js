44b3857bd8762ad2808167f9ff2f8ac29bdd11cb
Birje <birje.solatte@voco.ee>
2023-02-08 18:15:10 +0200

    Added handler

--- src/Components/Studies/StudyItem.js
+++ src/Components/Studies/StudyItem.js
@@ -1,15 +1,25 @@
 import "./StudyItem.css"
 import StudyDate from "./StudyDate";
+import "./StudyItem.css";
+import Card from "../UI/Card";
+import {useState} from "react";
 const StudyItem =  (props) => {
+    const [title, settitle] = useState(props.StudyData.title)
+
+    const clickHandler = (props) => {
+        settitle('Updated')
+        console.log(title)
+    }
 
     return (
-        <div className="study-item">
+        <Card className="study-item">
             <StudyDate date={props.StudyData.date}></StudyDate>
             <div className="study-item__description">
                 <h2>{props.StudyData.title}</h2>
                 <div className="study-item__list">{props.StudyData.list}</div>
             </div>
-        </div>
+            <button onClick = {clickHandler}>Change Title</button>
+        </Card>
     )
 }
 
